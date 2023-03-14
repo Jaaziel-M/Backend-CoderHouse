a0017b4d9f841ba6c3a50f131215608f211f38db
unknown <jaazi_nes@hotmail.com>
2023-03-14 18:17:09 -0300

    Entrega proyecto final 1

--- app.js
+++ app.js
@@ -1,121 +1,175 @@
 // Importo las dependencias al codigo
 const express = require('express');
+const {Server: HttpServer} = require('http');
+const {Server: ioServer} = require('socket.io');
 require('dotenv').config();
+const {v4 : uuidv4} = require('uuid');
+const ProdContainer = require('./src/Containers/containerProds');
+const login = require('./routes/login')
+const signIn = require('./routes/signIn')
+const errorSI = require('./routes/errorSignin');
+const errorLI = require('./routes/errorLogin');
+const home = require('./routes/home')
 const md5 = require('md5')
 const app = express()
-const cookieParser = require('cookie-parser');
+const http = new HttpServer(app); 
+const io = new ioServer(http); 
+const cookieParser = require('cookie-parser')
 const session = require('express-session');
-const COOKIE_SECRET = process.env.COOKIE_SECRET;
+const COOKIE_SECRET = process.env.COOKIE_SECRET
 const passport = require('passport');
-//const { Strategy } = require('passport-local');
 const LocalStrategy = require('passport-local').Strategy;
-
-//  Mongodb
+//      Mongo
 const mongoose = require('mongoose');
-const connect = require('./src/services/config/mongo-connection');
-connect()
 const MongoStore = require('connect-mongo');
-const ModelAuth = require('./src/services/models/model-auth')
-// Routes
-const error = require('./src/routes/error');
-const home = require('./src/routes/home');
-const login = require('./src/routes/login');
-const signin = require('./src/routes/signin');
-const logout = require('./src/routes/logout');
+const modelAuth = require('./src/Services/models/authModels')
 
-//
+const mail = require('./src/Services/config/mail').transport
+
 app.use(cookieParser(COOKIE_SECRET))
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ac-oxnem7u-shard-00-00.biltcc2.mongodb.net:27017,ac-oxnem7u-shard-00-01.biltcc2.mongodb.net:27017,ac-oxnem7u-shard-00-02.biltcc2.mongodb.net:27017/?ssl=true&replicaSet=atlas-14mqtq-shard-0&authSource=admin&retryWrites=true&w=majority`,
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true }
+    },
+    {
+    secret: COOKIE_SECRET,
+    rolling: true,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: false,
+        secure: false
+        //maxAge: 1000
+    }
+    })}))
 
-//  Route to views folder and ejs files
 app.set('views', "./public");
 app.set('view engine', 'ejs')
+let allmsgs = []
+let allprods = []
 app.use(express.static('public'))
+app.use('/home',home)
+app.use('/login',login)
+app.use('/signin',signIn)
+app.use('/errorSignin',errorSI)
+app.use('/errorLogin',errorLI)
 
-//  Session config with mongodb, IMPORTANT! WE MUST DEFINE APP SESSION BEFORE THE app.use('/error', error), .... otherwise it wont work 
-app.use(session(
-    {
-        store: MongoStore.create({
-            mongoUrl: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ac-oxnem7u-shard-00-00.biltcc2.mongodb.net:27017,ac-oxnem7u-shard-00-01.biltcc2.mongodb.net:27017,ac-oxnem7u-shard-00-02.biltcc2.mongodb.net:27017/?ssl=true&replicaSet=atlas-14mqtq-shard-0&authSource=admin&retryWrites=true&w=majority`,
-            mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true }
-        }),
-        secret: COOKIE_SECRET,
-        rolling: true,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            httpOnly: false,
-            secure: false
-            //maxAge: 1000
-        }
-    }))
-//
+Prod = new ProdContainer()
 
 
+
+//         HTTP METHODS 
+app.get('/errorSignin',(_req,res)=>{
+    res.redirect('/errorSignin')
+})
+
+app.get('/errorLogin',(_req,res)=>{
+    res.redirect('/errorLogin')
+})
 
 app.get('/health', (_req,res) => {
+    mail()
     res.status(200).json({
         "success": true,
         "health": "yes"
     })
+    
 })
 
-app.use('/error', error)
-app.use('/home', home)
-app.use('/signin', signin)
-app.use('/login', login)
+app.get('/',(req,res)=>{
+    const username = req.signedCookies.username
+    if(req.signedCookies.username != undefined){
+        return res.render('index.ejs',{username})
+    }
+    req.session.destroy(error => {
+        if(!error){
+            
+            return res.redirect('/login')
+        }
+    })
+})
 
-//  get to all our routes
-app.get('/error', (req,res)=>{res.redirect('/error')})
-app.get('/home', (req,res)=>{res.redirect('/home')})
-app.get('/login', (req,res)=>{res.redirect('/login')})
-app.get('/signin', (req,res)=>{res.redirect('/signin')})
-//
+app.get('/logout',(req,res)=>{
+    req.logout(()=> {
+        res.redirect('/login');
+    })
+})
 
-/////////////////////////   Passport config 
+app.get('/signin',(req,res)=>{
+    // if con res.redirect.isAuth para proteger rutas
+    res.redirect('/signin')
+})
+
+app.get('/login',(req,res)=>{
+    // if con res.redirect.isAuth para proteger rutas
+    res.redirect('/login')
+})
 
 
-//  Login function
-passport.use('login', new LocalStrategy(async (username, password, done) => {
-    //console.log(password);
-    //console.log(md5(password))
-    const userData = await ModelAuth.findOne({username, password: md5(password)});
-    console.log(userData)
-    if(!userData){
-        console.log("errpr")
-        return done(null, false)
+////////////PASSPORT 
+
+passport.use('login', new LocalStrategy(async (mail, password, done) => {
+    //      TRAIGO EL USUARIO DESDE MONGO
+    const userData = await modelAuth.findOne({ mail, password: md5(password) })
+    if (!userData) {
+        // DONE ES EL CALLBACK
+        try {
+            return done(null, false);
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
     done(null, userData)
 }))
-//  Signin function
-passport.use('signin', new LocalStrategy({passReqToCallback: true}, async (req, username, password, done) => {
-    console.log(username)
-    const userData = await ModelAuth.findOne({username, password: md5(password)})
-    console.log(userData)
-    if(userData){
+// {passReqToCallback: true},
+passport.use('signin', new LocalStrategy( async (req, username, mail, password, direccion, direccionNum, edad, tel, url, done) => {
+
+    const userData = await modelAuth.findOne({ username, password: md5(toString(password)) })
+    if (userData) {
         return done(null,false);
     }
-    const addNewUser = new ModelAuth({
-        username,
-        password: md5(password),
-        email: req.body.email
-    })
-    const saveUserToDb = await addNewUser.save();
-    done(null, saveUserToDb)
-}))
+    
+    const stageUser = new modelAuth({
+        nombre: toString(username),
+        email: toString(mail),
+        password: md5(toString(password)),
+        direccion: md5(toString(direccion)),
+        dirNum: md5(toString(direccionNum)),
+        edad: md5(toString(edad)),
+        tel: md5(toString(tel)),
+        url: md5(toString(url))
+    });
+    const newUser = await stageUser.save();
+    
+    try {
+        mail.sendMail(
+            {
+                from : 'Airsoft shop account',
+                to : toString(mail),
+                subject : 'Bienvenido/a a  Airsoft shop!',
+                html : `<h1>Hola!</h1><h2>Gracias por crearte una cuenta, te dejamos //los datos ingresados a continuacion:</h2><h3>Nombre: ${username}<///h3>Dirección: ${direccion}<h3>Edad: ${edad}</h3>Telefono de contacto: //${tel}<h3>Foto: ${url}</h3><h4>Gracias por elegirnos! </h4>`
+            }
+        ).then(items=>{console.log(items);done(null, newUser)})
+    } catch (error) {
+        console.log(error)
+    }    
+    done(null, newUser)
+}
 
-passport.serializeUser((user, done)=>{done(null,user._id)});
+));
+
+passport.serializeUser((user, done)=>{
+    done(null, user._id);
+})
 passport.deserializeUser(async (id, done)=>{
-    const userData = await ModelAuth.findById(id);
-    done(null,userData)
+    const userData = await modelAuth.findById(id);
+    done(null,userData);
 })
 app.use(passport.initialize());
 app.use(passport.session());
-// debemos llamar a logout despues de toda la config de passport
-app.use('/logout', logout)
+/////////////////////
 
-////////////////////////////////// 
-//  Use to get all the routes
-
-
-module.exports = app;
+module.exports = http;
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const http = require('./app');
 const Puerto = process.env.PORT || process.env.PORT2 
 http.listen(Puerto, ()=> {
     console.info(`Conectado en el puerto ${Puerto}`)
 })
+
